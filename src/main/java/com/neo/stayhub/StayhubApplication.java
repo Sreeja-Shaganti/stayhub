557fd2b209553062acb73ee72eb04ce93d848ea3
PD86161 <Pavan_Durgam@satven.com>
2026-04-01 15:48:39 +0530

    feat: Add application entry point and configuration - Add StayhubApplication main class - Add application.yml with Spring Boot configuration

---
+++ src/main/java/com/neo/stayhub/StayhubApplication.java
@@ -0,0 +1,14 @@
+package com.neo.stayhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class StayhubApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(StayhubApplication.class, args);
+    }
+
+}
