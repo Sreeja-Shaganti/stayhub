d90abb739834e1510f314c416785c0eb96a32ee5
PD86161 <Pavan_Durgam@satven.com>
2026-04-01 15:49:40 +0530

    feat: Add domain event listeners for cascade operations - Add event listeners for cascading delete operations - Support for cleaning up related data before entity deletion - Events for Amenity, Booking, Hotel, Payment, Role, Room, RoomType, and User

---
+++ src/main/java/com/neo/stayhub/events/BeforeDeleteAmenity.java
@@ -0,0 +1,13 @@
+package com.neo.stayhub.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public class BeforeDeleteAmenity {
+
+    private Long id;
+
+}
---
+++ src/main/java/com/neo/stayhub/events/BeforeDeleteRoomType.java
@@ -0,0 +1,13 @@
+package com.neo.stayhub.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public class BeforeDeleteRoomType {
+
+    private Long id;
+
+}
