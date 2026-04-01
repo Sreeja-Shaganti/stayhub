36784cf3604766914cfddc13e9477b1edf53c362
PD86161 <Pavan_Durgam@satven.com>
2026-04-01 15:48:50 +0530

    feat: Add Spring configuration classes - Add DomainConfig for domain model scanning - Add JacksonConfig for JSON serialization/deserialization

---
+++ src/main/java/com/neo/stayhub/config/DomainConfig.java
@@ -0,0 +1,28 @@
+package com.neo.stayhub.config;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+//import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@Configuration
+@EntityScan("com.neo.stayhub.domain")
+@EnableJpaRepositories("com.neo.stayhub.repos")
+@EnableTransactionManagement
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
+public class DomainConfig {
+
+    @Bean(name = "auditingDateTimeProvider")
+    public DateTimeProvider dateTimeProvider() {
+        return () -> Optional.of(OffsetDateTime.now());
+    }
+
+}
